ef25127550968fe356929fe4ac2d5377ac854340
Devon Faria <devonfaria@gmail.com>
2022-03-06 20:00:39 -0500

    created function that adds buttons to button-container with the name of the input text when search button is clicked

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,8 @@
 // Defining Interactables
 var cityInput = document.querySelector('.input-field');
 var searchBtn = document.querySelector('.search-btn');
-var totalSearches = JSON.parse(localStorage.getItem('searches')) || [];
+var totalSearches = [];
+// JSON.parse(localStorage.getItem('searches')) ||
 
 
 
@@ -14,8 +15,25 @@ var searchCity = function () {
   console.log(totalSearches);
   var storage = JSON.stringify(totalSearches);
   localStorage.setItem('searches', totalSearches);
-}
+  buttonCreation();
+};
+
+var buttonCreation = function () {
+  var container = document.querySelector('.button-container');
+  for (var i = 0; i < totalSearches.length; i++) {
+    var buttonField = document.createElement('button');
+    buttonField.classList.add('searched-city-button');
+    buttonField.textContent = totalSearches[i];
+    container.appendChild(buttonField);
+  };
+};
 
 // BUTTON FUNCTIONALITY
 // Search button
 searchBtn.addEventListener('click', searchCity);
+
+for (var i = 0; i < totalSearches.length; i++) {
+  var buttonField = document.createElement('button');
+  buttonField.classList.add('searched-city-button');
+  buttonField.textContent = totalSearches[i];
+};
